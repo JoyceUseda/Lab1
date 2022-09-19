a04d6814acb24efc666af486220837aacd92ba09
Andres Mejias <anmejiasma@est.utn.ac.cr>
2022-09-18 21:33:03 -0600

    Se eliminan metodos de abstractos de retiro, deposito, transferencia y se agrega metodo movimiento

--- src/Transacciones/Transacciones.java
+++ src/Transacciones/Transacciones.java
@@ -1,9 +1,8 @@
-
 package Transacciones;
 
+import Banco.Banco;
 import Clientes.Clientes;
-import Cuentas.CuentaDolares;
-import Cuentas.CuentaColones;
+import Cuentas.Cuenta;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -12,21 +11,17 @@ import java.util.Calendar;
  * @author andre
  */
 public abstract class Transacciones {
+
     Clientes cliente;
-    CuentaColones colones;
-    CuentaDolares dolares;
+    Cuenta ct;
+    Banco bc;
 
     protected int Id;
     protected String fecha;
     protected String cuenta;
-    protected String cuentadestino;
     protected float monto;
-    protected float saldo;
     protected float saldoanterior;
     protected float saldofinal;
-    protected float saldoanteriordestino;
-    protected float saldofinaldestino;
-    
 
     public int getId() {
         return Id;
@@ -40,27 +35,10 @@ public abstract class Transacciones {
         return cuenta;
     }
 
-    public String getCuentadestino() {
-        return cuentadestino;
-    }
-
     public double getMonto() {
         return monto;
     }
 
-    public void setMonto(float monto) {
-        this.monto = monto;
-    }
-
-    public float getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
-    
-    
     public double getSaldoanterior() {
         return saldoanterior;
     }
@@ -69,38 +47,16 @@ public abstract class Transacciones {
         return saldofinal;
     }
 
-    public double getSaldoanteriordestino() {
-        return saldoanteriordestino;
-    }
+    public abstract void Movimiento(String cuenta, float monto);
 
-    public double getSaldofinaldestino() {
-        return saldofinaldestino;
-    }
-      public abstract void TransferirColones(String cuenta, String cuentadestino, float monto);
-      public abstract void TransferirDolares(String cuenta, String cuentadestino, float monto);
-      public abstract void DepositoColones(String cuenta, float monto);
-      public abstract void DepositoDolares(String cuenta, float monto);
-//    public abstract void RetiroColones(String cuenta, double monto);
-//    public abstract void RetiroDolares()
-
-  
     public Transacciones(int Id, String cuenta, float monto) {
 
         this.Id = Id;
         this.fecha = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
         this.cuenta = cuenta;
+        this.saldoanterior = ct.getSaldo();
         this.monto = monto;
-//        this.saldoanterior = saldoanterior;
-//        this.saldofinal = saldofinal;
+        this.saldofinal = ct.getSaldo();
     }
-    public Transacciones(int Id, String cuentaorigen, String cuentadestino, float monto) {
-        this.Id = Id;
-        this.cuenta = cuentaorigen;
-        this.cuentadestino = cuentadestino;
-        this.monto = monto;
-//        this.saldoanterior = saldoanterior;
-//        this.saldofinal = saldofinal;
-//        this.saldoanteriordestino = saldoanteriordestino;
-//        this.saldofinaldestino = saldofinaldestino;
-    }
+
 }
