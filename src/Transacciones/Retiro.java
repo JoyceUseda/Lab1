ea639832e66454d1296823d06a44798b5bb9f2ef
Andres Mejias <anmejiasma@est.utn.ac.cr>
2022-09-18 21:30:32 -0600

    Se eliminan metodos de retiro colones y dolares y se agrega metodo movimiento

--- src/Transacciones/Retiro.java
+++ src/Transacciones/Retiro.java
@@ -1,7 +1,6 @@
 
 package Transacciones;
 
-import Transacciones.Transacciones;
 
 /**
  *
@@ -12,35 +11,12 @@ public class Retiro extends Transacciones {
     public Retiro(int Id, String cuenta, float monto) {
         super(Id, cuenta, monto);
     }
-    
-   public void RetiroColones(String cuenta, float monto) {
-        this.cuenta = cliente.buscarCuentaColones(cuenta);
-        this.saldoanterior = colones.getSaldo();
-        colones.movimientoRetiroColones(monto);
-        this.saldofinal = colones.getSaldo();
-    }
 
-    public void RetiroDolares(String cuenta, float monto) {
-        this.cuenta = cliente.buscarCuentaDolares(cuenta);
-        this.saldoanterior = colones.getSaldo();
-        dolares.movimientoRetiroDolares(monto);
-        this.saldofinal = colones.getSaldo();
+   @Override
+    public void Movimiento(String cuenta, float monto) {
+        this.cuenta = bc.buscarCuentas(cuenta);
+        this.saldoanterior = ct.getSaldo();
+        ct.movimientoRetiro(monto);
+        this.saldofinal = ct.getSaldo();
     }
-
-    @Override
-    public void TransferirColones(String cuenta, String cuentadestino, float monto) {
-    }
-
-    @Override
-    public void TransferirDolares(String cuenta, String cuentadestino, float monto) {
-    }
-
-    @Override
-    public void DepositoColones(String cuenta, float monto) {
-    }
-
-    @Override
-    public void DepositoDolares(String cuenta, float monto) {
-    }
-
 }
